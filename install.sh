667d91d03465018aa43adfdd4ab0ec70bb28306b
Jesse V. Burke <jburke@wapacklabs.com>
2015-08-31 18:51:03 -0400

    greenz
    do it

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 # Some dumb script by jburke@wapacklabs.com
 # Don't run this unless you like pizza.... 
 
-/usr/bin/afplay ~/Desktop/mac_bash/song.mp3&
-
-/*
+/usr/bin/afplay -v 10 song.mp3&
+GREEN='\033[0;0;32m'
+NC='\033[0m' # No Color
+printf "${GREEN}
 ──────────────────────▒████▒────────────
 ───────────────────░█████▓███░──────────
 ─────────────────░███▒░░░░░░██──────────
@@ -53,8 +54,17 @@
 ───▓█▒█▓▓▓▓▒▓██░─░▒░─────██▒░▓▓▓▒██─────
 ────████▓▓▓██░───────────▓█░▓▒░░▓█░─────
 ──────░█████░─────────────██▓─▒██░──────
-*/
-echo "Throwing a pizza party!"\n
+${NC}\n"
+echo "Throwing a pizza party!\n\n"
+echo "Do you like Pizza? (Y)es or (N)o?  Please enter your answer, followed by [ENTER]:"
+read input_variable
+if [ "$input_variable" == "Y" ]; then
+  echo "Continuing Install... \n"
+else
+  echo "Then dont use my scripts!\n"
+  pkill afplay
+  exit;
+fi
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 mkdir -p /Users/$USER/Library/Python/2.7/lib/python/site-packages
 echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> /Users/$USER/Library/Python/2.7/lib/python/site-packages/homebrew.pth
@@ -63,3 +73,4 @@ brew install nmap tmux vim git
 brew update && brew upgrade
 echo "colo slate
 syntax on" > ~/.vimrc
+pkill afplay
