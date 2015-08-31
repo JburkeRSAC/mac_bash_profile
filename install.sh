d5b280428bda98cf100e2811b1000d1a34562836
Jesse V. Burke <jburke@redskyalliance.org>
2015-08-31 10:49:10 -0400

    srlysly
    gimme pizza

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-mkdir -p /Users/scrape/Library/Python/2.7/lib/python/site-packages
-echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> /Users/scrape/Library/Python/2.7/lib/python/site-packages/homebrew.pth
-brew doctor -v
-brew install nmap tmux vim git
-brew update && brew upgrade
-echo "colo slate
-syntax on" > ~/.vimrc
